2168fef33166e3b13744b7d38504c0f23064b43a
Oscar <nike-o_94@hotmail.com>
2014-11-29 22:44:49 -0600

    Daos con sus funciones abstractas implementadas

--- src/Controlador/DAOS/DAOCandidato.java
+++ src/Controlador/DAOS/DAOCandidato.java
@@ -6,8 +6,10 @@
 package Controlador.DAOS;
 
 import DAO.DAOBD;
+import Prueba.Candidato;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -15,28 +17,60 @@ import java.sql.SQLException;
  */
 public class DAOCandidato extends DAOBD{
 
-    @Override
+     @Override
     public String obtenerCondicionElemento(Object elemento) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        int idCandidato = ((Candidato) elemento).getID();
+        String condicion = "candidato_id = " + idCandidato;
+
+        return condicion;
     }
 
     @Override
     public Object obtenerElementoDeTabla(ResultSet resultadoDeConsulta) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            return new Candidato(resultadoDeConsulta.getInt("candidato_id"),
+                    resultadoDeConsulta.getString("nombre"), resultadoDeConsulta.getInt("num_votos"));
+        } catch (SQLException ex) {
+            System.out.println("ERROR EN LA LEÍDA DE LA BD.");
+            ex.printStackTrace();
+        }
+        return null;
     }
 
     @Override
     public boolean updateElement(Object elemento, String condicion) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        this.establishConnection();
+
+        Candidato elementoAmodificar = (Candidato) elemento;
+
+        Statement sentencia = this.getConnection().createStatement();
+
+        int actualizaCandidato = sentencia.
+                executeUpdate("UPDATE mvcdb.candidato SET "
+                        + "`candidato_id` = '" + elementoAmodificar.getID() + "'"
+                        + ",`nombre` = '" + elementoAmodificar.getNombre() + "'"
+                        + ",`num_votos` = '" + elementoAmodificar.getNumVotos() + "'"
+                        + " WHERE " + condicion);
+
+        return (actualizaCandidato != 0);
     }
 
     @Override
     public Object findElement(String nombreTabla, String condicion) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    
+        this.establishConnection();
+        String query = "SELECT * FROM " + nombreTabla + " WHERE " + condicion;
 
-    
+        Statement sentenciaBuscaliente = this.getConnection().createStatement();
+        ResultSet busquedaCliente = sentenciaBuscaliente.executeQuery(query);
+        busquedaCliente.next();
+
+        Candidato unCandidato = new Candidato(busquedaCliente.getInt("candidato_id"),
+                busquedaCliente.getString("nombre"),
+                busquedaCliente.getInt("num_votos"));
+
+        return unCandidato;
+    } 
     
 }
