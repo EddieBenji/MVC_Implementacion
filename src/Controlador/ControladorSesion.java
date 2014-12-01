8b7ec9392e649180c6b349e4b69ea8d6ccfd16f0
Eduardo Reyes <Skaro@Eduardo.local>
2014-12-01 10:26:26 -0600

    code aesthetichs

--- src/Controlador/ControladorSesion.java
+++ src/Controlador/ControladorSesion.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controlador;
 
 import Fmat.Framework.Controlador.ClaseControlador;
@@ -11,26 +6,25 @@ import Modelo.AdminUsuario;
 
 /**
  *
- * @author Vanessa
+ * @author Vanessa Fragoso
  */
 public class ControladorSesion extends ClaseControlador {
 
-    public ControladorSesion(ClaseModelo modelo, int idEvento) {
-        super(modelo, idEvento);
+    public ControladorSesion( ClaseModelo modelo, int idEvento ) {
+        super( modelo, idEvento );
     }
-    
-    public boolean iniciarSesion(String usuario, String clave){
-        return (AdminUsuario.getInstancia()).iniciarSesion(usuario, clave);
+
+    public boolean iniciarSesion( String usuario, String clave ){
+        return ( AdminUsuario.getInstancia() ).iniciarSesion( usuario, clave );
     }
     public void cerrarSesion(){
-        (AdminUsuario.getInstancia()).cerrarSesion();
+        ( AdminUsuario.getInstancia() ).cerrarSesion();
     }
-    public void agregarCuenta(String usuario, String clave){
-        (AdminUsuario.getInstancia()).registrarCuenta(usuario, clave);
+    public void agregarCuenta( String usuario, String clave ){
+        ( AdminUsuario.getInstancia() ).registrarCuenta( usuario, clave );
     }
+
     @Override
-    public void actualizar(Object obj) {
-        //no implementado
-    }
-    
+    public void actualizar( Object obj ) {}
+
 }
